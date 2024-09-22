a70d4b2b3a644a8d7e7b7f74444fb72c8a71ca8c
Brayden Kelly <kel.20012@byui.edu>
2024-09-21 23:01:37 -0600

    Prep work completed for the week.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
+        static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Name and Number Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        int Number = Console.Read();
+        return Number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(int square, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(square, name);
     }
 }
