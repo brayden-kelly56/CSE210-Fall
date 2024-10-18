23975c8a0065d976bc79e9958bf5cd65b6686240
Brayden Kelly <kel.20012@byui.edu>
2024-10-17 20:25:56 -0600

    Learning Excercise Completion

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,53 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void GetTop()
+    {
+        Console.WriteLine($"{_top}");
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void GetBottom()
+    {
+        Console.WriteLine($"{_bottom}");
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string frac = $"{_top}/{_bottom}";
+        return frac;
+    }
+
+    public double GetDecimalValue()
+    {
+        double frac = (double)_top / (double)_bottom;
+        return frac;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction F1 = new Fraction();
+        Fraction F2 = new Fraction(5);
+        Fraction F3 = new Fraction(3, 4);
+
+
+        double decmial1 = F1.GetDecimalValue();
+        string frac1 = F1.GetFractionString();
+
+        double decmial2 = F2.GetDecimalValue();
+        string frac2 = F2.GetFractionString();
+
+        double decmial3 = F3.GetDecimalValue();
+        string frac3 = F3.GetFractionString();
+
+        Console.WriteLine($"{decmial1}");
+        Console.WriteLine($"{frac1}");
+
+        Console.WriteLine($"{decmial2}");
+        Console.WriteLine($"{frac2}");
+
+        Console.WriteLine($"{decmial3}");
+        Console.WriteLine($"{frac3}");
     }
 }
